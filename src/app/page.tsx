7f92b7e502b65a8ee62e9f941183b5ee24648a8e
Yavor Punchev <yavor.punchev@gmail.com>
2023-07-25 10:23:34 +0300

    Radix example code with exit transitions added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,224 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import React from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { styled, keyframes } from "@stitches/react";
+import { violet, blackA, mauve, green } from "@radix-ui/colors";
+import { Cross2Icon } from "@radix-ui/react-icons";
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+const DialogDemo = () => (
+  <Dialog.Root>
+    <Dialog.Trigger asChild>
+      <Button>Edit profile</Button>
+    </Dialog.Trigger>
+    <Dialog.Portal>
+      <DialogOverlay />
+      <TheDivInQuestion>
+        <DialogContent>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you&#x2019;re
+            done.
+          </DialogDescription>
+          <Fieldset>
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" defaultValue="Pedro Duarte" />
+          </Fieldset>
+          <Fieldset>
+            <Label htmlFor="username">Username</Label>
+            <Input id="username" defaultValue="@peduarte" />
+          </Fieldset>
+          <Flex css={{ marginTop: 25, justifyContent: "flex-end" }}>
+            <Dialog.Close asChild>
+              <Button variant="green">Save changes</Button>
+            </Dialog.Close>
+          </Flex>
+          <Dialog.Close asChild>
+            <IconButton aria-label="Close">
+              <Cross2Icon />
+            </IconButton>
+          </Dialog.Close>
+        </DialogContent>
+      </TheDivInQuestion>
+    </Dialog.Portal>
+  </Dialog.Root>
+);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+const fakeAnimation = keyframes({
+  from: { opacity: 1 },
+  to: { opacity: 1 },
+});
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+const fadeIn = keyframes({
+  from: { opacity: 0 },
+  to: { opacity: 1 },
+});
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+const fadeOut = keyframes({
+  from: { opacity: 1 },
+  to: { opacity: 0 },
+});
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+const zoomIn = keyframes({
+  from: { transform: "translate3d(-50%, 0, 0) scale(0.96)", opacity: 0 },
+  to: { transform: "translate3d(-50%, -50%, 0)  scale(1)", opacity: 1 },
+});
+
+const zoomOut = keyframes({
+  from: { transform: "translate3d(-50%, -50%, 0) scale(1)", opacity: 1 },
+  to: { transform: "translate3d(-50%, 0, 0) scale(0.96)", opacity: 0 },
+});
+
+const DialogOverlay = styled(Dialog.Overlay, {
+  backgroundColor: blackA.blackA9,
+  position: "fixed",
+  inset: 0,
+
+  "&[data-state='open']": {
+    animation: `${fadeIn} 400ms cubic-bezier(0.23, 1, 0.32, 1) forwards`,
+  },
+  "&[data-state='closed']": {
+    animation: `${fadeOut} 400ms cubic-bezier(0.23, 1, 0.32, 1) forwards`,
+  },
+});
+
+const DialogContent = styled(Dialog.Content, {
+  backgroundColor: "white",
+  borderRadius: 6,
+  boxShadow:
+    "hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px",
+  position: "fixed",
+  top: "50%",
+  left: "50%",
+  transform: "translate3d(0, 50%, 0) scale(0.96)",
+  width: "90vw",
+  maxWidth: "450px",
+  maxHeight: "85vh",
+  padding: 25,
+
+  "&[data-state='open']": {
+    animation: `${zoomIn} 400ms cubic-bezier(0.23, 1, 0.32, 1) forwards`,
+  },
+  "&[data-state='closed']": {
+    animation: `${zoomOut} 400ms cubic-bezier(0.23, 1, 0.32, 1) forwards`,
+  },
+
+  "&:focus": {
+    outline: "none",
+  },
+});
+
+const DialogTitle = styled(Dialog.Title, {
+  margin: 0,
+  fontWeight: 500,
+  color: mauve.mauve12,
+  fontSize: 17,
+});
+
+const DialogDescription = styled(Dialog.Description, {
+  margin: "10px 0 20px",
+  color: mauve.mauve11,
+  fontSize: 15,
+  lineHeight: 1.5,
+});
+
+// The div in question has to be Dialog.Overlay under the hood to be able to get
+// the data attributes needed to control the animations
+const TheDivInQuestion = styled(Dialog.Overlay, {
+  // This prevents the the div in questin from being unmounted until after this animation has completed
+  // The animation does nothing, but is necessary to allow the animations on the Modal Window to complete
+  "&[data-state='closed']": {
+    animation: `${fakeAnimation} 400ms cubic-bezier(0.23, 1, 0.32, 1) forwards`,
+  },
+});
+
+// Non-modal components and styles
+const Flex = styled("div", { display: "flex" });
+
+const Button = styled("button", {
+  all: "unset",
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: 4,
+  padding: "0 15px",
+  fontSize: 15,
+  lineHeight: 1,
+  fontWeight: 500,
+  height: 35,
+
+  variants: {
+    variant: {
+      violet: {
+        backgroundColor: "white",
+        color: violet.violet11,
+        boxShadow: `0 2px 10px ${blackA.blackA7}`,
+        "&:hover": { backgroundColor: mauve.mauve3 },
+        "&:focus": { boxShadow: `0 0 0 2px black` },
+      },
+      green: {
+        backgroundColor: green.green4,
+        color: green.green11,
+        "&:hover": { backgroundColor: green.green5 },
+        "&:focus": { boxShadow: `0 0 0 2px ${green.green7}` },
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: "violet",
+  },
+});
+
+const IconButton = styled("button", {
+  all: "unset",
+  fontFamily: "inherit",
+  borderRadius: "100%",
+  height: 25,
+  width: 25,
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: violet.violet11,
+  position: "absolute",
+  top: 10,
+  right: 10,
+
+  "&:hover": { backgroundColor: violet.violet4 },
+  "&:focus": { boxShadow: `0 0 0 2px ${violet.violet7}` },
+});
+
+const Fieldset = styled("fieldset", {
+  all: "unset",
+  display: "flex",
+  gap: 20,
+  alignItems: "center",
+  marginBottom: 15,
+});
+
+const Label = styled("label", {
+  fontSize: 15,
+  color: violet.violet11,
+  width: 90,
+  textAlign: "right",
+});
+
+const Input = styled("input", {
+  all: "unset",
+  width: "100%",
+  flex: "1",
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: 4,
+  padding: "0 10px",
+  fontSize: 15,
+  lineHeight: 1,
+  color: violet.violet11,
+  boxShadow: `0 0 0 1px ${violet.violet7}`,
+  height: 35,
+
+  "&:focus": { boxShadow: `0 0 0 2px ${violet.violet8}` },
+});
+
+export default DialogDemo;
